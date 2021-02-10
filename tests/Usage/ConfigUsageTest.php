c473cf69fcc9a4edc301181796ea363c29a4dbfe
Daniel Paul Carbone <daniel.p.carbone@gmail.com>
2021-02-09 23:55:43 -0600

    copyright update in tests

--- tests/Usage/ConfigUsageTest.php
+++ tests/Usage/ConfigUsageTest.php
@@ -6,7 +6,7 @@ use DCarbone\PHPConsulAPITests\ConsulManager;
 use PHPUnit\Framework\TestCase;
 
 /*
-   Copyright 2016-2018 Daniel Carbone (daniel.p.carbone@gmail.com)
+   Copyright 2016-2021 Daniel Carbone (daniel.p.carbone@gmail.com)
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
