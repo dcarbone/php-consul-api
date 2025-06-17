499467c6668e86359c51fecbdeb0ef964fe1a7a0
Daniel Carbone <daniel.p.carbone@gmail.com>
2025-06-17 15:32:52 -0500

    bit more done.

---
+++ src/ConfigEntry/InstanceLevelRateLimits.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DCarbone\PHPConsulAPI\ConfigEntry;
+
+/*
+   Copyright 2016-2025 Daniel Carbone (daniel.p.carbone@gmail.com)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+use DCarbone\PHPConsulAPI\AbstractModel;
+
+class InstanceLevelRateLimits extends AbstractModel
+{
+    public int $RequestsPerSecond;
+    public int $RequestsMaxBurst;
+    /** @var array<\DCarbone\PHPConsulAPI\ConfigEntry\InstanceLevelRouteRateLimits> */
+    public array $Routes;
+
+    public function __construct(
+        int $RequestsPerSecond = 0,
+        int $RequestsMaxBurst = 0,
+        array $Routes = []
+    ) {
+        $this->RequestsPerSecond = $RequestsPerSecond;
+        $this->RequestsMaxBurst = $RequestsMaxBurst;
+        $this->SetRoutes(...$Routes);
+    }
+
+    public function getRequestsPerSecond(): int
+    {
+        return $this->RequestsPerSecond;
+    }
+
+    public function setRequestsPerSecond(int $RequestsPerSecond): self
+    {
+        $this->RequestsPerSecond = $RequestsPerSecond;
+        return $this;
+    }
+
+    public function getRequestsMaxBurst(): int
+    {
+        return $this->RequestsMaxBurst;
+    }
+
+    public function setRequestsMaxBurst(int $RequestsMaxBurst): self
+    {
+        $this->RequestsMaxBurst = $RequestsMaxBurst;
+        return $this;
+    }
+
+    /**
+     * @return array<\DCarbone\PHPConsulAPI\ConfigEntry\InstanceLevelRouteRateLimits>
+     */
+    public function getRoutes(): array
+    {
+        return $this->Routes;
+    }
+
+    public function setRoutes(InstanceLevelRouteRateLimits ...$Routes): self
+    {
+        $this->Routes = $Routes;
+        return $this;
+    }
+
+    public static function jsonUnserialize(\stdClass $decoded, null|self $into = null): self
+    {
+        $n = $into ?? new self();
+        foreach ($decoded as $k => $v) {
+            if ('requests_per_second' === $k) {
+                $n->RequestsPerSecond = $v;
+            } elseif ('requests_max_burst' === $k) {
+                $n->RequestsMaxBurst = $v;
+            } elseif ('Routes' === $k) {
+                $n->Routes = [];
+                foreach ($v as $rv) {
+                    $n->Routes[] = InstanceLevelRouteRateLimits::jsonUnserialize($rv);
+                }
+            } else {
+                $n->{$k} = $v;
+            }
+        }
+        return $n;
+    }
+
+    public function jsonSerialize(): \stdClass
+    {
+        $out = new \stdClass();
+        foreach ($this->_getDynamicFields() as $k => $v) {
+            $out->{$k} = $v;
+        }
+        $out->RequestsPerSecond = $this->RequestsPerSecond;
+        $out->RequestsMaxBurst = $this->RequestsMaxBurst;
+        $out->Routes = $this->Routes;
+        return $out;
+    }
+}
---
+++ src/ConfigEntry/RateLimits.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DCarbone\PHPConsulAPI\ConfigEntry;
+
+/*
+   Copyright 2016-2025 Daniel Carbone (daniel.p.carbone@gmail.com)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+use DCarbone\PHPConsulAPI\AbstractModel;
+
+class RateLimits extends AbstractModel
+{
+    public InstanceLevelRateLimits $InstanceLevel;
+
+    public function __construct(null|InstanceLevelRateLimits $instanceLevel = null)
+    {
+        $this->InstanceLevel = $instanceLevel ?? new InstanceLevelRateLimits();
+    }
+
+    public function getInstanceLevel(): InstanceLevelRateLimits
+    {
+        return $this->InstanceLevel;
+    }
+
+    public function setInstanceLevel(InstanceLevelRateLimits $instanceLevel): self
+    {
+        $this->InstanceLevel = $instanceLevel;
+        return $this;
+    }
+
+    public static function jsonUnserialize(\stdClass $decoded, null|self $into = null): self
+    {
+        $n = $into ?? new self();
+        foreach ($decoded as $k => $v) {
+            if ('InstanceLevel' === $k || 'instance_level' === $k) {
+                $n->InstanceLevel = InstanceLevelRateLimits::jsonUnserialize($v);
+            } else {
+                $n->{$k} = $v;
+            }
+        }
+        return $n;
+    }
+
+    public function jsonSerialize(): \stdClass
+    {
+        $out = new \stdClass();
+        foreach ($this->_getDynamicFields() as $k => $v) {
+            $out->{$k} = $v;
+        }
+        $out->InstanceLevel = $this->InstanceLevel;
+        return $out;
+    }
+}
